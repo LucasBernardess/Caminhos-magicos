4aa2688d1674f2ec130ce45958b5af016e0556a3
MessiasFCM <messiasferes127@gmail.com>
2024-04-01 01:12:34 -0300

    Update

--- src/yen.c
+++ src/yen.c
@@ -4,21 +4,15 @@
 #include <limits.h>
 
 // Função auxiliar para remover uma aresta do grafo temporário
-void removerAresta(Grafo *grafo, int src, int dest)
-{
-    Aresta *anterior = NULL;
-    Aresta *atual = grafo->vertices[src].proxima;
+void removerAresta(Grafo* grafo, int src, int dest) {
+    Aresta* anterior = NULL;
+    Aresta* atual = grafo->vertices[src].proxima;
 
-    while (atual != NULL)
-    {
-        if (atual->destino == dest)
-        {
-            if (anterior == NULL)
-            {
+    while (atual != NULL) {
+        if (atual->destino == dest) {
+            if (anterior == NULL) {
                 grafo->vertices[src].proxima = atual->proxima;
-            }
-            else
-            {
+            } else {
                 anterior->proxima = atual->proxima;
             }
             free(atual);
@@ -29,41 +23,34 @@ void removerAresta(Grafo *grafo, int src, int dest)
     }
 }
 
-void restaurarGrafo(Grafo *grafo, Grafo *grafoTemporario)
-{
+void restaurarGrafo(Grafo* grafo, Grafo* grafoTemporario) {
     int numVertices = grafo->numVertices;
 
-    for (int i = 0; i < numVertices; i++)
-    {
+    for (int i = 0; i < numVertices; i++) {
         grafo->vertices[i].custo = grafoTemporario->vertices[i].custo;
         grafo->vertices[i].noAnterior = grafoTemporario->vertices[i].noAnterior;
 
         // Remove todas as arestas do grafo original
-        Aresta *atual = grafo->vertices[i].proxima;
-        while (atual != NULL)
-        {
-            Aresta *temp = atual;
+        Aresta* atual = grafo->vertices[i].proxima;
+        while (atual != NULL) {
+            Aresta* temp = atual;
             atual = atual->proxima;
             free(temp);
         }
         grafo->vertices[i].proxima = NULL;
 
         // Copia as arestas do grafo temporário de volta para o grafo original
-        Aresta *atualTemp = grafoTemporario->vertices[i].proxima;
-        Aresta *anterior = NULL;
-        while (atualTemp != NULL)
-        {
-            Aresta *novaAresta = (Aresta *)malloc(sizeof(Aresta));
+        Aresta* atualTemp = grafoTemporario->vertices[i].proxima;
+        Aresta* anterior = NULL;
+        while (atualTemp != NULL) {
+            Aresta* novaAresta = (Aresta*)malloc(sizeof(Aresta));
             novaAresta->destino = atualTemp->destino;
             novaAresta->peso = atualTemp->peso;
             novaAresta->proxima = NULL;
 
-            if (anterior == NULL)
-            {
+            if (anterior == NULL) {
                 grafo->vertices[i].proxima = novaAresta;
-            }
-            else
-            {
+            } else {
                 anterior->proxima = novaAresta;
             }
 
@@ -74,35 +61,29 @@ void restaurarGrafo(Grafo *grafo, Grafo *grafoTemporario)
 }
 
 // Função auxiliar para copiar um grafo para outro
-void copiarGrafo(Grafo *grafoOrigem, Grafo *grafoDestino)
-{
+void copiarGrafo(Grafo* grafoOrigem, Grafo* grafoDestino) {
     int numVertices = grafoOrigem->numVertices;
     grafoDestino->numVertices = numVertices;
-    grafoDestino->vertices = (Vertice *)malloc(numVertices * sizeof(Vertice));
+    grafoDestino->vertices = (Vertice*)malloc(numVertices * sizeof(Vertice));
 
-    for (int i = 0; i < numVertices; i++)
-    {
+    for (int i = 0; i < numVertices; i++) {
         grafoDestino->vertices[i].id = grafoOrigem->vertices[i].id;
         grafoDestino->vertices[i].custo = grafoOrigem->vertices[i].custo;
         grafoDestino->vertices[i].noAnterior = grafoOrigem->vertices[i].noAnterior;
         grafoDestino->vertices[i].proxima = NULL;
 
-        Aresta *atual = grafoOrigem->vertices[i].proxima;
-        Aresta *anterior = NULL;
+        Aresta* atual = grafoOrigem->vertices[i].proxima;
+        Aresta* anterior = NULL;
 
-        while (atual != NULL)
-        {
-            Aresta *novaAresta = (Aresta *)malloc(sizeof(Aresta));
+        while (atual != NULL) {
+            Aresta* novaAresta = (Aresta*)malloc(sizeof(Aresta));
             novaAresta->destino = atual->destino;
             novaAresta->peso = atual->peso;
             novaAresta->proxima = NULL;
 
-            if (anterior == NULL)
-            {
+            if (anterior == NULL) {
                 grafoDestino->vertices[i].proxima = novaAresta;
-            }
-            else
-            {
+            } else {
                 anterior->proxima = novaAresta;
             }
 
@@ -113,10 +94,8 @@ void copiarGrafo(Grafo *grafoOrigem, Grafo *grafoDestino)
 }
 
 // Função para liberar a memória alocada para os caminhos
-void liberarCaminhos(MenorCaminho *caminhos, int numCaminhos)
-{
-    for (int i = 0; i < numCaminhos; i++)
-    {
+void liberarCaminhos(MenorCaminho* caminhos, int numCaminhos) {
+    for (int i = 0; i < numCaminhos; i++) {
         free(caminhos[i].caminho);
     }
     free(caminhos);
@@ -167,26 +146,23 @@ void liberarCaminhos(MenorCaminho *caminhos, int numCaminhos)
 //     return caminhos;
 // }
 
-MenorCaminho encontrarCaminho(Grafo *grafo, int origem, int destino)
-{
+MenorCaminho encontrarCaminho(Grafo* grafo, int origem, int destino) {
     MenorCaminho caminho;
     caminho.custo = grafo->vertices[destino].custo;
     caminho.comprimentoCaminho = 0;
-
+    
     // Estimar o tamanho do caminho (isso pode ser ajustado conforme necessário)
-    caminho.caminho = (int *)malloc(grafo->numVertices * sizeof(int));
-
+    caminho.caminho = (int*)malloc(grafo->numVertices * sizeof(int));
+    
     int atual = destino;
-    while (atual != origem)
-    {
+    while(atual != origem) {
         caminho.caminho[caminho.comprimentoCaminho++] = atual;
         atual = grafo->vertices[atual].noAnterior;
     }
     caminho.caminho[caminho.comprimentoCaminho++] = origem;
 
     // Opcional: Inverter o caminho se necessário para que comece na origem e termine no destino
-    for (int i = 0; i < caminho.comprimentoCaminho / 2; i++)
-    {
+    for(int i = 0; i < caminho.comprimentoCaminho / 2; i++) {
         int temp = caminho.caminho[i];
         caminho.caminho[i] = caminho.caminho[caminho.comprimentoCaminho - 1 - i];
         caminho.caminho[caminho.comprimentoCaminho - 1 - i] = temp;
@@ -195,22 +171,18 @@ MenorCaminho encontrarCaminho(Grafo *grafo, int origem, int destino)
     return caminho;
 }
 
-void modificarGrafo(Grafo *grafo, MenorCaminho caminho)
-{
+void modificarGrafo(Grafo* grafo, MenorCaminho caminho) {
     // Exemplo: Remover a última aresta do caminho encontrado
-    if (caminho.comprimentoCaminho > 1)
-    {
+    if (caminho.comprimentoCaminho > 1) {
         int penultimoNo = caminho.caminho[caminho.comprimentoCaminho - 2];
         int ultimoNo = caminho.caminho[caminho.comprimentoCaminho - 1];
 
-        Aresta **ptr = &grafo->vertices[penultimoNo].proxima;
-        while (*ptr != NULL)
-        {
-            Aresta *atual = *ptr;
-            if (atual->destino == ultimoNo)
-            {
+        Aresta** ptr = &grafo->vertices[penultimoNo].proxima;
+        while (*ptr != NULL) {
+            Aresta* atual = *ptr;
+            if (atual->destino == ultimoNo) {
                 *ptr = atual->proxima; // Remover a aresta
-                free(atual);           // Liberar a memória da aresta removida
+                free(atual); // Liberar a memória da aresta removida
                 break;
             }
             ptr = &atual->proxima;
@@ -218,122 +190,29 @@ void modificarGrafo(Grafo *grafo, MenorCaminho caminho)
     }
 }
 
-// Supondo que você tem uma estrutura MenorCaminho e Aresta definida em algum lugar
+MenorCaminho* yen(Grafo* grafo, int origem, int destino, int k) {
+    MenorCaminho* caminhos = (MenorCaminho*)malloc(k * sizeof(MenorCaminho));
+    MenorCaminho caminho;
+    Grafo grafoTemp;
+    copiarGrafo(grafo, &grafoTemp);  // Copia o grafo original para o grafo temporário
 
-int caminhoJaEncontrado(ListaCaminhos* encontrados, MenorCaminho caminhoAtual) {
-    for (int i = 0; i < encontrados->numeroDeCaminhos; i++) {
-        if (compararCaminhos(&encontrados->caminhos[i], &caminhoAtual)) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-void adicionarCaminhoEncontrado(ListaCaminhos* encontrados, MenorCaminho caminhoAtual) {
-    encontrados->caminhos = realloc(encontrados->caminhos, (encontrados->numeroDeCaminhos + 1) * sizeof(MenorCaminho));
-    encontrados->caminhos[encontrados->numeroDeCaminhos] = caminhoAtual;  // Assumindo uma atribuição superficial aqui; pode ser necessário copiar membros profundamente
-    encontrados->numeroDeCaminhos++;
-}
-
-void liberarCaminho(MenorCaminho* caminho) {
-    free(caminho->caminho);  // Assumindo que caminho->caminho é um ponteiro para um array dinâmico
-    caminho->caminho = NULL;
-    caminho->comprimentoCaminho = 0;
-    caminho->custo = 0;
-}
-
-Aresta* encontrarArestasRemover(Grafo* copiaGrafo, MenorCaminho caminhoAtual) {
-    // Lista para armazenar as arestas a serem removidas
-    Aresta* arestasParaRemover = NULL;
-
-    // Percorrer o caminhoAtual do destino até a origem
-    int noAtual = caminhoAtual.destino;
-    while (noAtual != caminhoAtual.origem) {
-        int noAnterior = caminhoAtual.noAnterior[noAtual];
+    for (int i = 0; i < k; i++) {
+        dijkstra(&grafoTemp, origem);
+        caminho = encontrarCaminho(&grafoTemp, origem, destino);
         
-        // Encontrar a aresta que liga noAnterior a noAtual
-        Aresta* aresta = copiaGrafo->vertices[noAnterior].proxima;
-        while (aresta != NULL) {
-            if (aresta->destino == noAtual) {
-                // Esta aresta faz parte do caminho e deve ser removida
-                
-                // Criar uma nova Aresta para adicionar à lista de arestasParaRemover
-                Aresta* novaAresta = malloc(sizeof(Aresta));
-                novaAresta->origem = noAnterior;
-                novaAresta->destino = noAtual;
-                novaAresta->peso = aresta->peso; // Copiar o peso se necessário para sua lógica
-                novaAresta->proxima = arestasParaRemover;
-                arestasParaRemover = novaAresta;
-
-                break; // Parar a busca pois a aresta foi encontrada
-            }
-            aresta = aresta->proxima;
-        }
-
-        // Mover para o próximo nó no caminho
-        noAtual = noAnterior;
-    }
-
-    return arestasParaRemover;
-}
-
-
-void liberarArestas(Aresta* arestasRemover) {
-    // Percorra a lista ligada (ou estrutura de dados) de arestas e libere a memória
-    Aresta* atual = arestasRemover;
-    while (atual != NULL) {
-        Aresta* temp = atual;
-        atual = atual->proxima; // Assumindo que 'proxima' é o ponteiro para a próxima aresta na lista ligada
-        free(temp);
-    }
-}
-
-MenorCaminho *yen(Grafo *grafo, int origem, int destino, int k)
-{
-    MenorCaminho *caminhos = malloc(k * sizeof(MenorCaminho));
-    // Inicializar todos os custos como infinito ou um valor inválido para verificar depois
-    for (int i = 0; i < k; i++)
-    {
-        caminhos[i].custo = INT_MAX;
-        caminhos[i].caminho = NULL;
-        caminhos[i].comprimentoCaminho = 0;
-    }
-
-    ListaCaminhos encontrados;
-
-    for (int i = 0; i < k; i++)
-    {
-        Grafo copiaGrafo;
-        copiarGrafo(grafo, &copiaGrafo);
-
-        dijkstra(&copiaGrafo, origem);
-
-        MenorCaminho caminhoAtual = encontrarCaminho(&copiaGrafo, origem, destino);
-
-        // Verifique se o caminho atual já foi encontrado anteriormente
-        if (caminhoAtual.custo != INT_MAX && !caminhoJaEncontrado(&encontrados, caminhoAtual))
-        {
-            caminhos[i] = caminhoAtual;
-            adicionarCaminhoEncontrado(&encontrados, caminhoAtual);
-        }
-        else
-        {
-            // Se o caminho já foi encontrado ou é inválido, tente a próxima iteração
-            liberarCaminho(&caminhoAtual);
+        // Adicione uma verificação aqui para assegurar que o caminho é válido.
+        if (caminho.custo <= -1) {
+            // Se o custo é -1, não há caminho. Pule para o próximo caminho.
             continue;
+        }else{
+            caminhos[i] = caminho;
         }
-
-        // Encontre as arestas a serem removidas e modifique o grafo
-        Aresta *arestasRemover = encontrarArestasRemover(&copiaGrafo, caminhoAtual);
-
-        for (Aresta *a = arestasRemover; a != NULL; a = a->prox)
-        {
-            removerAresta(&copiaGrafo, a->origem, a->destino);
-        }
-
-        // Restaure o grafo original para a próxima iteração
-        liberarArestas(arestasRemover);
-        restaurarGrafo(&copiaGrafo, grafo);
+        
+        modificarGrafo(&grafoTemp, caminhos[i]);
+        
+        
+        // Restaure o grafo para a próxima iteração.
+        restaurarGrafo(grafo, &grafoTemp);
     }
 
     // Restaurar grafoTemp para o estado original se necessário
